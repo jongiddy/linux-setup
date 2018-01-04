d23e75269c0ab959eb551653f3031ca552ce354c
Jonathan Giddy <jongiddy@gmail.com>
2018-01-04 12:49:21 +0100

    Install SublimeText

--- setup.sh
+++ setup.sh
@@ -27,6 +27,23 @@ if [ -r /etc/os-release ]; then
 		fi
 		;;
 	esac
+        # Add SublimeText
+        case "${ID}" in
+        ubuntu)
+		if [ ! -r /usr/bin/subl ]
+		then
+			wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+			sudo apt-get install apt-transport-https
+			if [ ! -r /etc/apt/sources.list.d/sublime-text.list ]
+			then
+				echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+				sudo apt update
+			fi
+			sudo apt install -y sublime-text
+		fi
+                ;;
+        esac
+
 fi
 
 if [ ! `command -v git` ]; then
